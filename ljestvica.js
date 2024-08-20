6b45c728704ac1418f7ac92abc059eeffcae5600
TriTeam <triteam98@gmail.com>
2024-08-20 13:48:55 +0200

    Update ljestvica.js

--- ljestvica.js
+++ ljestvica.js
@@ -18,29 +18,29 @@ onValue(utrke, (snapshot) => {
         lista1.push(Object.values(rezultat[i][1]))
        
         
-}
-let lista = lista1.sort((a, b) => b[1] - a[1]);
-console.log(lista)
-for(let i=0;i<lista.length;i++){
-    let redak=document.createElement('div');
-    let osoba=document.createElement('span');
-    let vrijednost=document.createElement('span');
-    let kojije=document.createElement('span')
-    kojije.className='kojije'
-    redak.className='redak'
-    osoba.className='osoba'
-    vrijednost.className='vrijednost'
-    osoba.innerHTML=lista[i][0]
-    vrijednost.innerHTML=lista[i][1]
-    kojije.innerHTML=Number([i])+1+'.'
-    
-    poredak.append(redak)
-    redak.append(kojije)
-    redak.append(osoba)
-    
-    redak.append(vrijednost)
+    }
+    let lista = lista1.sort((a, b) => b[1] - a[1]);
+    console.log(lista)
+    for(let i=0;i<lista.length;i++){
+        let redak=document.createElement('div');
+        let osoba=document.createElement('span');
+        let vrijednost=document.createElement('span');
+        let kojije=document.createElement('span')
+        kojije.className='kojije'
+        redak.className='redak'
+        osoba.className='osoba'
+        vrijednost.className='vrijednost'
+        osoba.innerHTML=lista[i][0]
+        vrijednost.innerHTML=lista[i][1]
+        kojije.innerHTML=Number([i])+1+'.'
+        
+        poredak.append(redak)
+        redak.append(kojije)
+        redak.append(osoba)
+        
+        redak.append(vrijednost)
 
-}
-
+    }
+    document.querySelector('.wrapper').style.display = 'none';
 })
 
